0b147fcc216863ac3c56e1e9b0be562e93b68628
JanaHazem12 <jana.hazem.abbass@gmail.com>
2025-01-17 00:02:04 +0200

    LAB 1 Submission

---
+++ LAB 1/portofolio/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, AboutMeComponent, SkillsComponent, ProjectsComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'portofolio';
+}
